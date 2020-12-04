df411ee5271f3f2367686a2ac3984128ae65a4ed
Vincent Frascello <vincent.frascello@gmail.com>
2020-12-04 00:25:11 -0800

    FizzBuzz Challenge - updated without If/Else

--- IdeaProjects/FizzBuzz2/src/com/company/Main.java
+++ IdeaProjects/FizzBuzz2/src/com/company/Main.java
@@ -5,12 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	for (int i=1;i<=100;i++)
     {
-        switch (i%5){
-            case 0: {
-                if (i%3==0){System.out.println("BrendenShilling");}else{System.out.println("Shilling");}
+        switch (i%15){
+            case 0: System.out.println("BrendenShilling");
                 break;
-            }
-            default: if(i%3==0){System.out.println("Brenden");}else{System.out.println(i);} break;
+            case 3: case 6: case 9: case 12: System.out.println("Brenden"); break;
+
+            case 5: case 10: System.out.println("Shilling"); break;
+            default: System.out.println(i); break;
         }
     }
     }
